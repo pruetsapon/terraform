924c65a1f392b66fc7795d06f0927a5fbd1c1108
unknown <pruetsapon.xuz@gmail.com>
2021-11-29 16:59:02 +0700

    use sudo su as inline.

--- remote-exec/main.tf
+++ remote-exec/main.tf
@@ -11,6 +11,11 @@ resource "null_resource" "shell" {
       # configure selinux as permissive
       "sudo setenforce 0",
       "sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config",
+      # use sudo su as psql
+      "sudo su - postgres -c 'psql <<EOF",
+      "create database test;",
+      "drop database test;",
+      "EOF'",
     ]
     connection {
       type = "ssh"
